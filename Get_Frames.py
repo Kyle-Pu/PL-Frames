284f41f4428244f08d99e520540f558a3a5ea1bc
Kyle Pu <kylez.pu@gmail.com>
2021-04-17 17:19:36 -0700

    Frames

---
+++ Get_Frames.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+imageDirectory = "Frames" # Directory to store image output
+
+# Create out directory if nonexistent
+isDir = os.path.isdir(imageDirectory)
+if not isDir:
+	os.mkdir(imageDirectory)
+
+# Create sub-directories in output folder if nonexistent
+angles = [-9, -4, -2, 0, 2, 4, 9]
+for angle in angles:
+	directoryPath = f"{imageDirectory}/%s" % angle
+	if not os.path.isdir(directoryPath):
+		os.mkdir(directoryPath)
+
+# We want frames from all videos
+for vidFilePath in os.listdir("./Videos"):
+	videoCapture = cv2.VideoCapture(f"./Videos/{vidFilePath}") # Start video capture
+
+    # Keep extracting frames until end of video
+	frameNum = 1 # For labeling frames
+	while True:
+		hasFramesLeft, image = videoCapture.read()   # Grab next video frame
+
+		if not hasFramesLeft:
+			break
+
+		cv2.imwrite(f"./{imageDirectory}/{vidFilePath[0:vidFilePath.find('.')]}/{frameNum}.jpg", image) # Save frame as JPEG file, find folder via video name
+		frameNum += 1
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Frames
 
-Original data not my own.
+Decompose videos into frames. Original data not my own.
 
 ## References
 
 https://link.springer.com/content/pdf/10.3758/BF03206542.pdf
 
 https://jov.arvojournals.org/article.aspx?articleid=2121553
+
+https://github.com/github/gitignore/blob/master/Global/macOS.gitignore
 
